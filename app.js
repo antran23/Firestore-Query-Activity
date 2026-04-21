06f7f8e32388d7af194c599446696154369cc5e9
angela <angela.tran123@gmail.com>
2026-04-21 14:52:07 -0500

    to add to collection

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const team3 = {name: 'Manchester United', city: 'Manchester', country: 'England'
 const team4 = {name: 'Manchester City', city: 'Manchester', country: 'England', topscorers: ['Sterling','Aguero','Haaland'],WorldwideFans: 537}
 const team5 = {name: 'Brazil National Team', city: 'N/A', country: 'Brazil', topscorers: ['Ronaldinho','Cafu','Bebeto'],WorldwideFans: 950}
 const team6 = {name: 'Argentina National Team', city: 'N/A', country: 'Argentina', topscorers: ['Messi','Batistuta','Maradona'],WorldwideFans: 888}
-const team7 = {name: 'Athletico Madrid', city: 'Madrid', country: 'Spain', topscorers: ['Aragonés','Griezmann','Torez'],WorldwideFans: 400}
-const teams = [team1, team2, team3, team4, team5, team6, team7];
-
+const team7 = {name: 'Atletico Madrid', city: 'Madrid', country: 'Spain', topscorers: ['Aragonés','Griezmann','Torez'],WorldwideFans: 400}
+teams.forEach(team => {
+  db.collection("teams").add(team)
+    .then(() => {
+      console.log(team.name + " added");
+    })
+    .catch(error => {
+      console.error("Error adding team: ", error);
+    });
+});
