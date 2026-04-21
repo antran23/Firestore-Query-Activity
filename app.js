b3443935aba7362a4884fd720e8e86e584ba47bd
angela <angela.tran123@gmail.com>
2026-04-21 14:13:46 -0500

    added all teams

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const team1 = {name: 'Real Madrid', city: 'Madrid', country: 'Spain', topscorers: ['Ronaldo','Benzema','Hazard'],WorldwideFans: 798}
+const team2 = {name: 'Barcelona', city: 'Barcelona', country: 'Spain', topscorers: ['Messi','Suarez','Puyol'],WorldwideFans: 738}
+const team3 = {name: 'Manchester United', city: 'Manchester', country: 'England', topscorers: ['Cantona','Rooney','Ronaldo'],WorldwideFans: 755}
+const team4 = {name: 'Manchester City', city: 'Manchester', country: 'England', topscorers: ['Sterling','Aguero','Haaland'],WorldwideFans: 537}
+const team5 = {name: 'Brazil National Team', city: 'N/A', country: 'Brazil', topscorers: ['Ronaldinho','Cafu','Bebeto'],WorldwideFans: 950}
+const team6 = {name: 'Argentina National Team', city: 'N/A', country: 'Argentina', topscorers: ['Messi','Batistuta','Maradona'],WorldwideFans: 888}
+const team7 = {name: 'Athletico Madrid', city: 'Madrid', country: 'Spain', topscorers: ['Aragonés','Griezmann','Torez'],WorldwideFans: 400}
